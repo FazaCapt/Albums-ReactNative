0511c26a50738f9715b845bb19c863616e0559e8
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-10 09:56:52 +0700

    List Component Boilerplate

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,21 @@
 
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
 
 // Create  a component
 
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+    
+
+    // return 1;
+    // return 2;
   );
 
 // render it to the device
