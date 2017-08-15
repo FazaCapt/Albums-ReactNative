3e6918a3609b61e614066e5a21191a031ae17100
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-16 06:46:33 +0700

    Styling a Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,25 @@ import { View } from 'react-native';
 
 const Card = () => {
     return (
-        <View></View>
-    )
-}
+        <View style={styles.constainerStyle}></View>
+    );
+};
+
+const styles = {
+    constainerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowOffset: { width: 0, height: 2 },
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadawRadius: 2,
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
+};
 
 export default Card;
