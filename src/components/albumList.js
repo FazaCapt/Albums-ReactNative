3d04cd9ca20151d81bb99eeb65cf6eac5040411b
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-15 20:04:02 +0700

    component Level State, Belum bisa

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -3,11 +3,18 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component { 
+    state = { albums: [] };
+
     componentWillMount() {
         axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => console.log(response));
-    }
+            .then(response => this.setState({ albums: response.data }));
+
+            // this.state = { albums: [ {}{}{}]}
+        }
+    
+
     render() {
+        console.log(this.state);
     return (
         <View>
             <Text>Album List!!!</Text>
